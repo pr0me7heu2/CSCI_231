66fd9f8f6ac4bf06a73a8cb3b5e7999ef058ce9c
Bryan <brbrasher@gmail.com>
2020-02-05 21:02:21 -0500

    Initial commit of project 1.

---
+++ Project01/BrasherBryanProject01.java
@@ -0,0 +1,23 @@
+
+/**
+ * Project 01
+ * Brasher, Bryan R.
+ * CSCI 321
+ */
+public class BrasherBryanProject01
+{
+
+    public static void main(String [] args) {
+        System.out.println("Hello, World!");
+    
+        System.out.println("This is my first Java Program.");
+    }
+   
+// changing to system - error: package system does not exist
+
+// breaking line of code - error: ; expected
+
+// changing first System.out.println call to .print - causes "This is my
+// "first Java Program." to print on the same line
+
+}
