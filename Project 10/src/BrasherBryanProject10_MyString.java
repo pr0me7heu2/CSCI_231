c63e3b7c675342b2e37145122fa99e8776e406b7
Bryan <brbrasher@gmail.com>
2020-04-21 21:54:53 -0400

    Created and completed Project 10

---
+++ Project 10/src/BrasherBryanProject10_MyString.java
@@ -0,0 +1,122 @@
+import org.jetbrains.annotations.Contract;
+
+public class BrasherBryanProject10_MyString {
+
+    private char[] data;
+
+    //creates MyString object
+    public BrasherBryanProject10_MyString(char[] chars) {
+        this.data = chars;
+    }
+
+    // returns char at index i in data
+    // throws exception if is less than zero or
+    // greater than the largest index in data
+    public char charAt(int i) {
+        if(i >= 0 && i < data.length) {
+            return data[i];
+        }
+        // outside of data
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // returns length of data
+    public int length() {
+        return data.length;
+    }
+
+    // returns MyString object with data containing
+    // subset of calling object data from index begin
+    // through (and including) index end
+    public BrasherBryanProject10_MyString substring(int begin, int end) {
+        if(begin >= 0 && end < data.length) {
+            char[] mySubChar = new char[end - begin + 1];
+            for (int i = begin; i < end + 1 ; i++) {
+                mySubChar[i - begin] = data[i];
+            }
+            return new BrasherBryanProject10_MyString(mySubChar);
+        }
+        // outside of data
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // returns a MyString object equivalent to
+    // calling object but with all upper case ASCII
+    // characters in this.data replaced with their
+    // lower case equivalent
+    public BrasherBryanProject10_MyString toLowerCase() {
+        char[] myLowerChar = new char[this.length()];
+
+        for (int i = 0; i < this.length(); i++) {
+            //if data[i] is uppercase ASCII character
+            if(this.data[i] >= 65 && this.data[i] <= 90) {
+                myLowerChar[i] = (char) (this.data[i] + 32);
+            }
+            else {
+                myLowerChar[i] = this.data[i];
+            }
+        }
+        return new BrasherBryanProject10_MyString(myLowerChar);
+    }
+
+    // returns true if and only if other.data == this.data
+    public boolean equals(BrasherBryanProject10_MyString other) {
+        // if different length, false
+        if (this.length() != other.length()) {
+            return false;
+        }
+        // else must be identical for each index
+        // e.g. this.data[i] == other.data[i] for all i
+        else {
+            for (int i = 0; i < this.length(); i++) {
+                if(other.charAt(i) != this.data[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // returns MyString object whose data member
+    // contains the individual base-10 representations
+    // of int i e.g,, 10423 becomes {'1','0','4','2','3'}
+   public static BrasherBryanProject10_MyString valueOf(int i) {
+        //first need to determine "length" of i
+        //while preserving the value of i
+       int intCount = 0;
+       int tempi = i;
+       while(tempi > 0) {
+           tempi /= 10;
+           intCount++;
+       }
+       //create int array to hold individual numbers of i
+       int[] intArr = new int[intCount];
+
+       //fill int array with individual digits of i
+       for (int j = 0; j < intCount; j++) {
+           intArr[j] = i % 10;
+           i /= 10;
+       }
+
+       // fill a char array with members of intArr
+       char[] intCharArr = new char[intCount];
+       int charCount = 0;  // needed to cycle up through char array
+       for (int j = intCount -1; j >= 0 ; j--) {
+           intCharArr[charCount] = (char)(intArr[j] + 48); //convert int to char
+           charCount++;
+       }
+
+       return new BrasherBryanProject10_MyString(intCharArr);
+
+   }
+
+   // getter function
+   public char[] getData() {
+        return this.data;
+   }
+
+}
---
+++ Project 10/src/MyStringTestDrive.java
@@ -0,0 +1,54 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+
+public class MyStringTestDrive {
+
+    public static void main(String[] args) {
+
+        System.out.println("Driver for BrasherBryanProject10_MyString");
+
+        //scanner object
+        Scanner input = new Scanner(System.in);
+
+//        //random object
+//        Random rand = new Random();
+//
+//        //tests constructor
+//        System.out.println("Enter a line of text: ");
+//        BrasherBryanProject10_MyString myString = new BrasherBryanProject10_MyString(input.nextLine().toCharArray());
+//        System.out.println("You entered: " + Arrays.toString(myString.getData()));
+//
+//        //test charAt(int i)
+//        int randomIndex = rand.nextInt(myString.getData().length + 1);
+//        System.out.println("The character at index " + randomIndex + " is: " + myString.charAt(randomIndex));
+//
+//        //test length()
+//        System.out.println("The length of string is: " + myString.length());
+//
+//        //test substring(int begin, int end))
+//        System.out.println("Enter beginning and  indexes for substring: e.g. \"3 7\"");
+//        int begin = input.nextInt();
+//        int end = input.nextInt();
+//        System.out.println("Substring is: " + Arrays.toString(myString.substring(begin,end).getData()));
+//
+//        //test to toLowerCase()
+//        System.out.println("That as a lowercase MyString object: ");
+//        System.out.println(Arrays.toString(myString.toLowerCase().getData()));
+//
+//        //test equals(MyString other)
+//        input.nextLine();  //advances scanner to next line
+//        System.out.println("Enter another line of text: ");
+//        System.out.println("Your original string is identical to your new string: " +
+//                myString.equals(new BrasherBryanProject10_MyString(input.nextLine().toCharArray())));
+
+        //test valueOf(int i)
+        System.out.println("Enter an integer: ");
+        System.out.println("That as a MyString object is: " +
+                Arrays.toString(BrasherBryanProject10_MyString.valueOf(input.nextInt()).getData()));
+
+
+    }
+}
